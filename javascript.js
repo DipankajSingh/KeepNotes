c149fbe1f19df5c0dd185abbf7c942cb9aaae4ed
Dipankaj Singh <dipankajsingh2@gmail.com>
2022-04-06 13:24:28 +0530

    dark theme fully added

--- javascript.js
+++ javascript.js
@@ -132,7 +132,6 @@ document.querySelector('.dark-theme-label').addEventListener('click',()=>{
   let checkbox=document.querySelector('.dark-theme-checkbox');
   // there is false working like true
   if (checkbox.checked==false) {
-    console.log('initial check!');
     setValue('background-color','hsl(282, 50%, 3.9%)');
     setValue('ui-text-color','hsl(284, 76.8%, 64.5%)');
     setValue('alfa','1')
@@ -148,4 +147,22 @@ document.querySelector('.dark-theme-label').addEventListener('click',()=>{
     setValue('shadow','0 0 0.5rem 0.1rem hsla(275.6, 85%, 26.1%, var(--alfa1))');
     setValue('back-active-color','hsla(275.6, 85%, 26.1%, var(--alfa1))')
     setValue('border-color','hsl(284, 76.8%, 74.5%)')
+}
+
+if (checkbox.checked==true) {
+  setValue('background-color','hsl(111, 33%, 8%)');
+    setValue('ui-text-color','hsl(120, 100%, 50%)');
+    setValue('alfa','1')
+    setValue('notes-background-color','hsla(120, 100%, 50%, var(--alfa)');
+    setValue('alfa1','calc(var(--alfa) - .1)')
+    setValue('notes-color','hsl(100, 3%, 23%)');
+    setValue('icon-color','hsl(0, 0%, 100%)');
+    setValue('notes-hover-color','hsl(96, 100%, 50%)');
+    setValue('scrollbar-color','hsl(120, 90%, 15%)');
+    setValue('dailog-color','hsla(120, 78%, 49%, 0.986)')
+    setValue('icon-hover-color','hsl(110, 98%, 48%)')
+    setValue('icon-done-hover-color','hsl(110, 64%, 24%)')
+    setValue('shadow','0 0 0.5rem 0.1rem hsla(120, 79%, 21%, var(--alfa1))');
+    setValue('back-active-color','hsla(120, 79%, 21%, var(--alfa1))')
+    setValue('border-color','rgb(21, 59, 4)')
 }})
