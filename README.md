c750a3600f35cdb98b7be42ee1bb55ce206e1d2d
Dipankaj Singh <dipankajsingh2@gmail.com>
2022-04-15 17:23:00 +0530

    initial comkit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # KeepNotes
+
+const button =document.getElementById('button');
+const jokeText=document.getElementById('paragraph');
+
+
+addEventListener('DOMContentLoaded',getJoke)
+button.addEventListener('click',getJoke);
+
+function getJoke(){
+    fetch('https://icanhazdadjoke.com/',{
+        headers:{
+            'accept':'application/json'
+        } }).then(data=>data.json()).then(obj=>jokeText.innerHTML=obj.joke)
+}
--- test.js
+++ test.js
@@ -6,3 +6,41 @@ arr.find((value,index)=>{
 })
 
 console.log(arr);
+const setValue = (target, value) =>
+  root.style.setProperty("--" + target, value);
+
+const darkTheme = () => {
+  setValue("background-color", "hsl(282, 50%, 3.9%)");
+  setValue("ui-text-color", "hsl(284, 76.8%, 64.5%)");
+  setValue("alfa", "1");
+  setValue("notes-background-color", "hsla(275.6, 85%, 26.1%, var(--alfa))");
+  setValue("alfa1", "calc(var(--alfa) - .1)");
+  setValue("notes-color", "hsl(284, 76.8%, 64.5%)");
+  setValue("icon-color", "hsl(284, 76.8%, 64.5%)");
+  setValue("notes-hover-color", "hsl(284.1, 100%, 25.9%)");
+  setValue("scrollbar-color", "var(--ui-text-color)");
+  setValue("dailog-color", "hsl(275.6, 85%, 35.1%)");
+  setValue("icon-hover-color", "hsl(275.6, 85%, 75.1%)");
+  setValue("icon-done-hover-color", "var(--icon-hover-color)");
+  setValue("shadow", "0 0 0.5rem 0.1rem hsla(275.6, 85%, 26.1%, var(--alfa1))");
+  setValue("back-active-color", "hsla(275.6, 85%, 26.1%, var(--alfa1))");
+  setValue("border-color", "hsl(284, 76.8%, 74.5%)");
+};
+
+const lightTheme = () => {
+  setValue("background-color", "hsl(111, 33%, 8%)");
+  setValue("ui-text-color", "hsl(120, 100%, 50%)");
+  setValue("alfa", "1");
+  setValue("notes-background-color", "hsla(120, 100%, 50%, var(--alfa)");
+  setValue("alfa1", "calc(var(--alfa) - .1)");
+  setValue("notes-color", "hsl(100, 3%, 23%)");
+  setValue("icon-color", "hsl(0, 0%, 100%)");
+  setValue("notes-hover-color", "hsl(96, 100%, 50%)");
+  setValue("scrollbar-color", "hsl(120, 90%, 15%)");
+  setValue("dailog-color", "hsla(120, 78%, 49%, 0.986)");
+  setValue("icon-hover-color", "hsl(110, 98%, 48%)");
+  setValue("icon-done-hover-color", "hsl(110, 64%, 24%)");
+  setValue("shadow", "0 0 0.5rem 0.1rem hsla(120, 79%, 21%, var(--alfa1))");
+  setValue("back-active-color", "hsla(120, 79%, 21%, var(--alfa1))");
+  setValue("border-color", "rgb(21, 59, 4)");
+};
